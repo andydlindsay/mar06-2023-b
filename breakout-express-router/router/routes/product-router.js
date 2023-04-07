09a8c0a159a464d5b12863dd5efc9a244e22c1da
Andy Lindsay <andydlindsay@gmail.com>
2023-04-07 13:46:32 -0700

    breakout over

---
+++ breakout-express-router/router/routes/product-router.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const productQueries = require('../db/product-queries');
+
+router.use((req, res, next) => {
+  // if (!req.cookies.user_id) {
+  //   return res.redirect('/login');
+  // }
+  console.log('inside the products router');
+
+  next();
+});
+
+// GET /products/
+router.get('/', (req, res) => {
+  productQueries.getProducts()
+    .then((products) => {
+      res.json(products);
+    });
+});
+
+// GET /products/:id
+router.get('/:id', (req, res) => {
+  const productId = req.params.id;
+  productQueries.getProductById(productId)
+    .then((product) => {
+      res.json(product);
+    });
+});
+
+
+module.exports = router;
